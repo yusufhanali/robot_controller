0e59a255b74a9dcbeb26a6cb110f602f387aa29d
yusufhanali <yusufhanali22@gmail.com>
2025-01-26 18:03:52 +0300

    .

--- robot_controller/new_controller.py
+++ robot_controller/new_controller.py
@@ -11,10 +11,7 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
-from .jacobian import get_jacobian
-
-joint_states_global = {}
-    
+from .jacobian import get_jacobian    
 
 class NewController(Node):
 
@@ -23,25 +20,28 @@ class NewController(Node):
         self.get_logger().info('New Controller Node Started')
         self.joint_state_sub = self.create_subscription(JointState, 'joint_states', self.joint_state_callback, 10)
         
+        self.joint_states_global = {}
+        
         self.velocityControllerTopic = "forward_velocity_controller/commands"
         self.velocityControllerPub = self.create_publisher(Float64MultiArray, self.velocityControllerTopic, 10)
         
         self.tfBuffer = Buffer()
         self.tfListener = TransformListener(self.tfBuffer, self)
         
-        self.create_timer(1/100, self.get_gaze)
+        self.create_timer(1/1000, self.maain)
                         
         print("sanity")        
         
     def maain(self):
-        vel = np.array([0, 0, 0, 0, 0.01, 0])
-        comm = self.get_inverse_jacobian() @ vel
-        print(comm)
-        self.publishVelocityCommand(comm)
+        try:
+            vel = np.array([0, 0.01, 0, 0, 0, 0])
+            comm = self.get_inverse_jacobian() @ vel
+            self.publishVelocityCommand(comm)
+        except:
+            print("Error in main")
                         
     def get_inverse_jacobian(self):
-        global joint_states_global
-        jacobian = get_jacobian(joint_states_global["pos"][0], joint_states_global["pos"][1], joint_states_global["pos"][2], joint_states_global["pos"][3], joint_states_global["pos"][4], joint_states_global["pos"][5])
+        jacobian = get_jacobian(self.joint_states_global["pos"][0], self.joint_states_global["pos"][1], self.joint_states_global["pos"][2], self.joint_states_global["pos"][3], self.joint_states_global["pos"][4], self.joint_states_global["pos"][5])
         invj = np.linalg.pinv(jacobian, rcond=1e-15)
         return invj
         
@@ -51,22 +51,21 @@ class NewController(Node):
         self.velocityControllerPub.publish(vel_msg)
         
     def joint_state_callback(self, msg):
-        global joint_states_global
-        joint_states_global["pos"] = np.array([
+        self.joint_states_global["pos"] = np.array([
                                             msg.position[5],
                                             msg.position[0],
                                             msg.position[1],
                                             msg.position[2],
                                             msg.position[3],
                                             msg.position[4]])
-        joint_states_global["vels"] = np.array([
+        self.joint_states_global["vels"] = np.array([
                                             msg.velocity[5],
                                             msg.velocity[0],
                                             msg.velocity[1],
                                             msg.velocity[2],
                                             msg.velocity[3],
                                             msg.velocity[4]])
-        joint_states_global["effs"] = np.array([
+        self.joint_states_global["effs"] = np.array([
                                             msg.effort[5],
                                             msg.effort[0],
                                             msg.effort[1],
